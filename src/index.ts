76d3cab1bdf5a1c2d89397149f837c228d7b5d8e
Lorenzo C <lorenzo@deepcellbio.com>
2022-12-18 22:42:54 -0300

    deployment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config();
-import Fastify from "fastify";
+import Fastify, { FastifyInstance } from "fastify";
 import { routes } from "./routes";
 import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import cors from "@fastify/cors";
@@ -10,13 +10,21 @@ import { checkEnvVars } from "./utils/checkEnvVars";
 // https://www.fastify.io/docs/latest/Guides/Serverless/#google-cloud-run
 // For more details, please visit the URL above.
 
-function build() {
-    const fastify = Fastify({ trustProxy: true, logger: false }); // Must trust proxy for Google Cloud Run
-    const server = fastify.withTypeProvider<TypeBoxTypeProvider>();
+function build(): FastifyInstance {
+    const _server = Fastify({ trustProxy: true, logger: false }); // Must trust proxy for Google Cloud Run
+    const server = _server.withTypeProvider<TypeBoxTypeProvider>();
+    server.register(cors, { origin: ["http://localhost:3000"] });
+    server.get("/", {}, () => {
+        return { status: "Healthy" };
+    });
+    server.register(routes, { prefix: "/api/v1" });
+    checkEnvVars(); // Checks if environment variables are properly set
     return server;
 }
 
 async function start() {
+    const server = build();
+
     // Google Cloud Run will set this environment variable, so
     // we can also use it to detect whether we're running in Cloud Run
     const IS_GOOGLE_CLOUD_RUN = process.env.K_SERVICE !== undefined;
@@ -27,20 +35,8 @@ async function start() {
     // We must listen on all IPV4 addresses in Cloud Run
     const host = IS_GOOGLE_CLOUD_RUN ? "0.0.0.0" : undefined;
 
-    const fastify = build();
-
-    fastify.register(cors, { origin: ["http://localhost:3000"] });
-
-    fastify.get("/", {}, () => {
-        return { status: "Healthy" };
-    });
-
-    fastify.register(routes, { prefix: "/api/v1" });
-
-    checkEnvVars(); // Checks if environment variables are properly set
-
     try {
-        const address = await fastify.listen({ host, port });
+        const address = await server.listen({ host, port });
         console.log(`Server running on: ${address}`);
     } catch (err) {
         console.error(err);
@@ -51,3 +47,5 @@ async function start() {
 if (require.main === module) {
     start();
 }
+
+export { build };
